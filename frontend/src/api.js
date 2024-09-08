fe0489a90b98023a6cc91abf8182cb75a388423d
AFRAAD-PC1 <AFRAAD-PC1@DESKTOP-TLU6IP4>
2024-09-08 12:29:04 +0300

    "Add backend URL"

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,10 @@
 import axios from "axios"
 import { ACCESS_TOKEN } from "./constants"
 
+const apiUrl = "/choreo-apis/handelingnotesfullstack/backend/v1"
+
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
+    baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
 });
 
 
