8a436d3c0cbd34d003cfc00a70410c8f33057591
ziad <ziadelsayedhamed@gmail.com>
2024-09-20 19:57:49 +0300

    edit side bar

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -14,7 +14,7 @@ function LandingPage() {
     slidesToScroll: 1,
     arrows: false,
     autoplay: true,
-    autoplaySpeed: 3000,
+    autoplaySpeed: 2000,
   };
 
   return (
@@ -52,7 +52,7 @@ function LandingPage() {
         </div>
         </header>
     
-
+       
 
         {/* The top section */}
         <section className="relative bg-body overflow-hidden">
@@ -87,52 +87,166 @@ function LandingPage() {
             {/* Mobile Nav */}
             {mobileNavOpen && (
             <div className="fixed top-0 left-0 bottom-0 w-5/6 max-w-md z-50">
-                <div onClick={() => setMobileNavOpen(!mobileNavOpen)} className="fixed inset-0 bg-violet-600 opacity-20"></div>
-                <nav className="relative flex flex-col py-7 px-10 w-full h-full bg-black overflow-y-auto">
-                <div className="flex mb-auto items-center">
+                <div
+                onClick={() => setMobileNavOpen(!mobileNavOpen)}
+                className="fixed inset-0 bg-violet-600 opacity-20"
+                ></div>
+                <nav className="relative flex flex-col justify-start py-7 px-10 w-full h-full bg-black overflow-y-auto">
+                <div className="flex items-center">
                     <Link className="inline-block mr-auto" to="/">
                     <img className="h-10" src="casper-assets/logos/casper-logo.svg" alt="" />
                     </Link>
+                     {/* Logo Container */}
+                    <div className="flex justify-start w-full">
+                    <a href="/login" className="flex justify-center">
+                        <img src="/images/3.png" alt="Logo" className="w-32 rounded" />
+                    </a>
+                    </div>
+
                     <button onClick={() => setMobileNavOpen(!mobileNavOpen)}>
-                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M6 18L18 6M6 6L18 18" stroke="#F86E00" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round"></path>
+                    <svg
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                        d="M6 18L18 6M6 6L18 18"
+                        stroke="#F86E00"
+                        strokeWidth="3"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        ></path>
                     </svg>
                     </button>
                 </div>
-                <div className="py-12 mb-auto">
-                    <ul className="flex-col">
-                    <li className="mb-6">
+
+                <div className="py-12">
+                    <nav className="mt-10">
+                        
+                    <a
+                        className="flex items-center px-6 py-2 mt-4 text-gray-100 bg-gray-700 bg-opacity-25"
+                        href="#"
+                    >
+                        <svg
+                        className="w-6 h-6"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M11 3.055A9.001 9.001 0 1020.945 13H11V3.055z"
+                        ></path>
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M20.488 9H15V3.512A9.025 9.025 0 0120.488 9z"
+                        ></path>
+                        </svg>
+                        
+                        <span className="mx-3">
                         <Link className="inline-block text-base text-white font-medium uppercase" to="/featured">
-                        FEATURED
+                            FEATURED
                         </Link>
-                    </li>
-                    <li className="mb-6">
+                        </span>
+                    </a>
+                    <a
+                        className="flex items-center px-6 py-2 mt-4 text-gray-500 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100"
+                        href="#"
+                    >
+                        <svg
+                        className="w-6 h-6"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M17 14v6m-3-3h6M6 10h2a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2zm10 0h2a2 2 0 002-2V6a2 2 0 00-2-2h-2a2 2 0 00-2 2v2a2 2 0 002 2zM6 20h2a2 2 0 002-2v-2a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2z"
+                        ></path>
+                        </svg>
+                        <span className="mx-3">
                         <Link className="inline-block text-base text-white font-medium uppercase" to="/solutions">
-                        SOLUTIONS
+                            SOLUTIONS
                         </Link>
-                    </li>
-                    <li className="mb-6">
+                        </span>
+                    </a>
+                    <a
+                        className="flex items-center px-6 py-2 mt-4 text-gray-500 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100"
+                        href="#"
+                    >
+                        <svg
+                        className="w-6 h-6"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"
+                        ></path>
+                        </svg>
+                        <span className="mx-3">
                         <Link className="inline-block text-base text-white font-medium uppercase" to="/products">
-                        PRODUCTS
+                            PRODUCTS
                         </Link>
-                    </li>
-                    <li>
+                        </span>
+                    </a>
+                    <a
+                        className="flex items-center px-6 py-2 mt-4 text-gray-500 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100"
+                        href="#"
+                    >
+                        <svg
+                        className="w-6 h-6"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
+                        ></path>
+                        </svg>
+                        <span className="mx-3">
                         <Link className="inline-block text-base text-white font-medium uppercase" to="/articles">
-                        ARTICLES
+                            ARTICLES
                         </Link>
-                    </li>
-                    </ul>
+                        </span>
+                    </a>
+                    </nav>
                 </div>
-                <div>
-                    <Link className="flex py-2 px-4 mb-4 items-center justify-center text-sm font-medium uppercase text-orange-500 hover:text-orange-900" to="/login">
-                    SIGN IN
-                    </Link>
-                    <Link className="flex h-11 py-2 px-4 items-center justify-center text-sm font-medium uppercase text-black hover:text-white bg-orange-500 hover:bg-orange-600 transition duration-200 rounded-full" to="/register">
-                    SIGN UP
-                    </Link>
-                </div>
+                <div className="mt-auto">
+                <Link
+                className="flex py-2 px-4 mb-4 items-center justify-center text-sm font-medium uppercase text-orange-500 hover:text-orange-900"
+                to="/login"
+                >
+                SIGN IN
+                </Link>
+                <Link
+                className="flex h-11 py-2 px-4 items-center justify-center text-sm font-medium uppercase text-black hover:text-white bg-orange-500 hover:bg-orange-600 transition duration-200 rounded-full"
+                to="/register"
+                >
+                SIGN UP
+                </Link>
+                 </div>
                 </nav>
             </div>
+
             )}
         </section>
         {/*How it Works */}
@@ -322,8 +436,8 @@ function LandingPage() {
                 <div className="flex items-center justify-between mt-4">
                     {/* h4 and span on the left */}
                     <div className="text-left">
-                    <h4 className="text-xl text-orange-500 font-semibold">Andrew Garfield</h4>
-                    <span className="text-sm text-white">CTO of Mexant</span>
+                    <h4 className="text-xl text-orange-500 font-semibold">Nader Hani</h4>
+                    <span className="text-sm text-white">Co-Founder of CodeWave</span>
                     </div>
 
                     {/* Image on the right */}
@@ -348,8 +462,8 @@ function LandingPage() {
                 <div className="flex items-center justify-between mt-4">
                     {/* h4 and span on the left */}
                     <div className="text-left">
-                    <h4 className="text-xl text-orange-500 font-semibold">Andrew Garfield</h4>
-                    <span className="text-sm text-white">CTO of Mexant</span>
+                    <h4 className="text-xl text-orange-500 font-semibold">Fras Mohamed</h4>
+                    <span className="text-sm text-white">Co-Founder of CodeWave</span>
                     </div>
 
                     {/* Image on the right */}
@@ -374,8 +488,8 @@ function LandingPage() {
                 <div className="flex items-center justify-between mt-4">
                     {/* h4 and span on the left */}
                     <div className="text-left">
-                    <h4 className="text-xl text-orange-500 font-semibold">Andrew Garfield</h4>
-                    <span className="text-sm text-white">CTO of Mexant</span>
+                    <h4 className="text-xl text-orange-500 font-semibold">Mustafa Esmail</h4>
+                    <span className="text-sm text-white">Co-Founder of CodeWave</span>
                     </div>
 
                     {/* Image on the right */}
@@ -400,8 +514,8 @@ function LandingPage() {
                 <div className="flex items-center justify-between mt-4">
                     {/* h4 and span on the left */}
                     <div className="text-left">
-                    <h4 className="text-xl text-orange-500 font-semibold">Andrew Garfield</h4>
-                    <span className="text-sm text-white">CTO of Mexant</span>
+                    <h4 className="text-xl text-orange-500 font-semibold">Ziad Elasyed</h4>
+                    <span className="text-sm text-white">Co-Founder of CodeWave</span>
                     </div>
 
                     {/* Image on the right */}
